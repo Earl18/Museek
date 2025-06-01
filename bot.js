0bd28658b978d339e18378858b07f5f2e9aea8f5
Earl Justine Sierra <104546797+Earl18@users.noreply.github.com>
2025-06-01 18:27:34 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const { executablePath } = require('yt-dlp-exec');
 const { DisTube } = require('distube');
 const { SpotifyPlugin } = require('@distube/spotify');
 const { YtDlpPlugin } = require('@distube/yt-dlp');
@@ -56,7 +57,7 @@ client.distube = new DisTube(client, {
             update: true,
             quality: 'highestaudio',
             highWaterMark: 1 << 25, // Higher buffer to reduce lag
-            executable: 'yt-dlp'
+            executable: executablePath
         })
     ],
 });
